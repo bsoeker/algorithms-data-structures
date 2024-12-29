4ed774c96990226389a7c389aff29d631269910f
Barlas Söker <bsoker98@gmail.com>
2024-12-29 20:55:03 +0300

    Implement remove methods of the linked list class and also the size()

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 public class Main {
 
-	public static void main(String[] args) {
-		MyLinkedList<Integer> list = new MyLinkedList<>();
-		// list.addLast(10);
-		// list.addLast(20);
-		list.addLast(30);
-		list.addFirst(5);
-		list.addFirst(1);
-		System.out.println(list);
-		System.out.println(list.indexOf(1));
-		System.out.println(list.indexOf(5));
-		System.out.println(list.contains(30));
-	}
+    public static void main(String[] args) {
+        MyLinkedList<Integer> list = new MyLinkedList<>();
+        // list.addLast(10);
+        // list.addLast(20);
+        // list.addLast(30);
+        // list.addLast(40);
+        // list.addLast(10);
+        System.out.println(list.size());
+
+        System.out.println(list);
+    }
 }
--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,76 +1,127 @@
 public class MyLinkedList<T> {
 
-	private class Node {
-		T value;
-		Node next;
+    private class Node {
+        T value;
+        Node next;
 
-		private Node(T value) {
-			this.value = value;
-			this.next = null;
-		}
-	}
+        private Node(T value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
 
-	private Node first;
-	private Node last;
+    private Node first;
+    private Node last;
 
-	public void addLast(T value) {
-		Node node = new Node(value);
+    private boolean isEmpty() {
+        return first == null;
+    }
 
-		if (first == null)
-			first = last = node;
-		else {
-			last.next = node;
-			last = node;
-		}
-	}
+    public void addLast(T value) {
+        Node node = new Node(value);
 
-	public void addFirst(T value) {
-		Node node = new Node(value);
+        if (isEmpty())
+            first = last = node;
+        else {
+            last.next = node;
+            last = node;
+        }
+    }
 
-		if (first == null)
-			first = last = node;
-		else {
-			node.next = first;
-			first = node;
-		}
+    public void addFirst(T value) {
+        Node node = new Node(value);
 
-	}
+        if (isEmpty())
+            first = last = node;
+        else {
+            node.next = first;
+            first = node;
+        }
 
-	public int indexOf(T value) {
-		Node current = first;
-		int index = 0;
+    }
 
-		while (current != null) {
-			if (current.value == value)
-				return index;
-			current = current.next;
-			index++;
-		}
+    public int indexOf(T value) {
+        Node current = first;
+        int index = 0;
 
-		return -1;
-	}
+        while (current != null) {
+            if (current.value == value)
+                return index;
+            current = current.next;
+            index++;
+        }
 
-	public boolean contains(T value) {
-		return indexOf(value) != -1;
-	}
+        return -1;
+    }
 
-	@Override
-	public String toString() {
-		if (first == null)
-			return "[ ]";
+    public boolean contains(T value) {
+        return indexOf(value) != -1;
+    }
 
-		StringBuffer stringBuffer = new StringBuffer();
-		stringBuffer.append("[ ");
+    public void removeFirst() {
+        // [first -> second -> third]
+        if (isEmpty()) {
+            System.out.println("List is empty!");
+            return;
+        }
 
-		Node current = first;
-		while (current != null) {
-			stringBuffer.append(current.value + ", ");
-			current = current.next;
-		}
+        Node second = first.next;
+        first.next = null;
+        first = second;
+    }
 
-		stringBuffer.deleteCharAt(stringBuffer.length() - 1 - 1);
-		stringBuffer.append("]");
+    public void removeLast() {
+        // [first -> second -> third -> fourth]
+        if (isEmpty()) {
+            System.out.println("List is empty!");
+            return;
+        }
 
-		return stringBuffer.toString();
-	}
+        if (first == last) {
+            first = last = null;
+            return;
+        }
+
+        Node current = first;
+        while (current.next != last) // We have to get to the second to last item
+            current = current.next;
+
+        current.next = null;
+        last = current;
+    }
+
+    public int size() {
+        if (isEmpty())
+            return 0;
+
+        int counter = 0;
+        Node current = first;
+
+        while (current != null) {
+            current = current.next;
+            counter++;
+        }
+
+        return counter;
+    }
+
+    @Override
+    public String toString() {
+        if (isEmpty())
+            return "[ ]";
+
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("[ ");
+
+        Node current = first;
+        while (current != null) {
+            stringBuffer.append(current.value + ", ");
+            current = current.next;
+        }
+
+        stringBuffer.deleteCharAt(stringBuffer.length() - 1 - 1);
+        stringBuffer.append("]");
+
+        return stringBuffer.toString();
+    }
 }
