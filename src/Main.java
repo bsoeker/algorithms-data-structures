fe4d9c594932d8ca2c7b67161ae93dc1b99112cb
Barlas Söker <bsoker98@gmail.com>
2025-01-20 00:46:42 +0300

    Sorting example using a heap

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         Heap heap = new Heap();
-        heap.insert(15);
-        heap.insert(10);
-        heap.insert(3);
-        heap.insert(8);
-        heap.insert(12);
-        heap.insert(9);
-        heap.insert(4);
-        heap.insert(1);
-        heap.insert(24);
-        heap.remove();
-        heap.remove();
-        heap.remove();
+        List<Integer> numbers = List.of(1, 2, 43, 4536, 123, 567, 45, 26, 756);
+        for (Integer number : numbers) {
+            heap.insert(number);
+        }
 
-        System.out.println(heap);
+        List<Integer> sorted = new ArrayList<>();
+        while (!heap.isEmpty())
+            sorted.addFirst(heap.remove());
+        // or use sorted.add(heap.remove()) for descending order!
+
+        System.out.println(sorted);
 
     }
 }
